dc9c16a20fe56049238bd2f3294897b4a1c7166c
Jeremy <mail.jeremy.li@gmail.com>
2022-08-31 11:29:10 +1000

    added describe blocks

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,50 +1,53 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-it('renders same text passed into title prop', () => {
-    const title = 'Make Michael think he\'s the one in the bad place';
-    render(<Header title={title} />);
-    const h1 = screen.getByText(title)
-    expect(h1).toBeInTheDocument();
-});
+describe('Header', () => {
 
-it('renders an element with heading role', () => {
-    const title = 'Make Michael think he\'s the one in the bad place';
-    render(<Header title={title} />);
-    // we specify the title so that if there are multiple headings,
-    // we only grab the one with the right title instead of throwing an error
-    const heading = screen.getByRole('heading', { name: title });
-    expect(heading).toBeInTheDocument();
-});
-
-// this should only be used as last resort
-it('renders an element with the right test id', () => {
-    const title = 'Make Michael think he\'s the one in the bad place';
-    render(<Header title={title} />);
-    const heading = screen.getByTestId('header-1')
-    expect(heading).toBeInTheDocument();
-});
-
-// Find By
-it('should render same text passed into title prop', async () => {
-    const title = 'Make Michael think he\'s the one in the bad place';
-    render(<Header title={title} />);
-    const heading = await screen.findByText(title)
-    expect(heading).toBeInTheDocument();
-});
-
-// Query By
-it('should not render dogs', () => {
-    const title = 'Make Michael think he\'s the one in the bad place';
-    render(<Header title={title} />);
-    const heading = screen.queryByText(/dogs/i)
-    expect(heading).not.toBeInTheDocument();
-});
-
-// Get All By
-it('should render only one heading', async () => {
-    const title = 'Make Michael think he\'s the one in the bad place';
-    render(<Header title={title} />);
-    const headingElements = screen.getAllByRole('heading')
-    expect(headingElements.length).toBe(1);
+    it('renders same text passed into title prop', () => {
+        const title = 'Make Michael think he\'s the one in the bad place';
+        render(<Header title={title} />);
+        const h1 = screen.getByText(title)
+        expect(h1).toBeInTheDocument();
+    });
+    
+    it('renders an element with heading role', () => {
+        const title = 'Make Michael think he\'s the one in the bad place';
+        render(<Header title={title} />);
+        // we specify the title so that if there are multiple headings,
+        // we only grab the one with the right title instead of throwing an error
+        const heading = screen.getByRole('heading', { name: title });
+        expect(heading).toBeInTheDocument();
+    });
+    
+    // this should only be used as last resort
+    it('renders an element with the right test id', () => {
+        const title = 'Make Michael think he\'s the one in the bad place';
+        render(<Header title={title} />);
+        const heading = screen.getByTestId('header-1')
+        expect(heading).toBeInTheDocument();
+    });
+    
+    // Find By
+    it('should render same text passed into title prop', async () => {
+        const title = 'Make Michael think he\'s the one in the bad place';
+        render(<Header title={title} />);
+        const heading = await screen.findByText(title)
+        expect(heading).toBeInTheDocument();
+    });
+    
+    // Query By
+    it('should not render dogs', () => {
+        const title = 'Make Michael think he\'s the one in the bad place';
+        render(<Header title={title} />);
+        const heading = screen.queryByText(/dogs/i)
+        expect(heading).not.toBeInTheDocument();
+    });
+    
+    // Get All By
+    it('should render only one heading', async () => {
+        const title = 'Make Michael think he\'s the one in the bad place';
+        render(<Header title={title} />);
+        const headingElements = screen.getAllByRole('heading')
+        expect(headingElements.length).toBe(1);
+    });
 });
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,21 +10,23 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
     );
 }
 
-it('renders the correct amount of incomplete tasks', () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-    const taskText = screen.getByText('5 tasks left');
-    expect(taskText).toBeInTheDocument();
+describe('TodoFooter', () => {
+    it('renders the correct amount of incomplete tasks', () => {
+        render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+        const taskText = screen.getByText('5 tasks left');
+        expect(taskText).toBeInTheDocument();
 
-    // more assertions
-    // expect(taskText).toBeVisible();
-    // expect(taskText).toContainHTML('p');
-    // expect(taskText).toHaveTextContent('5 tasks left');
-    // expect(taskText.textContent).toBe('5 tasks left');
-});
+        // more assertions
+        // expect(taskText).toBeVisible();
+        // expect(taskText).toContainHTML('p');
+        // expect(taskText).toHaveTextContent('5 tasks left');
+        // expect(taskText.textContent).toBe('5 tasks left');
+    });
 
-it('renders the render "task" when the number of incomplete tasks is 1', () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-    const taskText = screen.getByText('1 task left');
-    expect(taskText).toBeInTheDocument();
-    // expect(taskText).toBeVisible();
+    it('renders the render "task" when the number of incomplete tasks is 1', () => {
+        render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+        const taskText = screen.getByText('1 task left');
+        expect(taskText).toBeInTheDocument();
+        // expect(taskText).toBeVisible();
+    });
 });
